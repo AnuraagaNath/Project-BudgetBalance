a57c26aa2019c75ac534fb985ef09c77fee2742a
AnuraagaNath <anuraaga.oct15@gmail.com>
2024-06-26 15:47:23 +0530

    update

--- __init__.py
+++ __init__.py
@@ -1,2 +1,2 @@
-from dotenv import load_dotenv
-from gsheet import Data, CreateTable, Updater
+from gsheet import Data
+from gsheet import Updater
--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import streamlit as st
-from gsheet import CreateTable, Updater
+from gsheet import Updater
 
 
 
